2f298e15d0874b0cd4dd21cc357fa8c710dbce86
erriomina-git <erriomina@gmail.com>
2025-05-08 18:38:46 +0500

    Update Pizzacard.js

--- components/Pizzacard.js
+++ components/Pizzacard.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './Pizzacard.css';
+import Counter from "./Counter";
 
 function ToDo(props) {
     return (
         <div className="blocks">
             <div className="block">
                 <img src={props.img} alt="img" className='img' />
-                <h3>{props.text}</h3>
+                <h2>{props.text}</h2>
                 <p>Ничего лишнего! Томатный соус, колбаски</p>
-                <p>Пепперони И Моцарелла</p>
+                <p className='p2'>Пепперони И Моцарелла</p>
                 <form>
                     <label for="diamenet"></label>
                     <select id="diameter" name="diamener">
-                        <option disabled selected>Диаметр</option>
+                        <option disabled selected>  Диаметр</option>
                         <option value="30">30см</option>
                         <option value="35">35см</option>
                         <option value="40">40см</option>
                     </select>
                 </form>
-                <h2>от 550₽</h2>
+                <h1>от 550₽</h1>
+                <Counter />
             </div>
         </div>
     )
